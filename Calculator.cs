0722366ef009e2d4bb90630e2c118c030d282ae4
kimmskiee <kimberlykenn.bontuyan@gmail.com>
2026-03-28 09:13:37 +0800

    Calculator.cs

--- Calculator.cs
+++ Calculator.cs
@@ -1,12 +1,32 @@
 using System;
 
-public static class Calculator
+public class Calculator 
 {
-    public static double? Calculate(int first, int second, string operation)
-    {
-        //write your business logic in here
 
-        return null;
-    }
+    public static object Calculate(int n1, int n2, string op)
+    {
+        switch (op)
+        {
+            case "+":
+                return n1 + n2;
+            case "-":
+                return n1 - n2;
+            case "*":
+                return n1 * n2;
+            case "/":
+                if (n2 == 0) {
+                    Console.WriteLine("Cannot divide by zero");
+                    return null;
+                }
+                return (double)n1 / n2;
+            case "%":
+                if (n2 == 0) {
+                    Console.WriteLine("Cannot modulo by zero");
+                    return null;
+                }
+                return n1 % n2;
+            default:
+                return null;
+        }
+    }
 }
-
